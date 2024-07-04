4fa4d5ff08a37d3cbd5a274624f70327d39c1410
makinde olasubomi <121644515+Shubbin@users.noreply.github.com>
2024-07-04 20:33:38 +0100

    adjust the ratio

--- slider.js
+++ slider.js
@@ -1,25 +1,29 @@
 document.addEventListener('DOMContentLoaded', function() {
-    let currentImage = 0;
-    const images = [
-      'image1.jpg', 'image2.jpg', 'image3.jpg', 'image4.jpg', 'image5.jpg',
-      'image6.jpg', 'image7.jpg', 'image8.jpg', 'image9.jpg', 'image10.jpg'
-    ];
-    const sliderImage = document.getElementById('sliderImage');
-    const moreSurpriseButton = document.getElementById('moreSurpriseButton');
+  let currentImage = 0;
+  const images = [
+    'image1.jpg', 'image2.jpg', 'image3.jpg', 'image4.jpg', 'image5.jpg',
+    'image6.jpg', 'image7.jpg', 'image8.jpg', 'image9.jpg', 'image10.jpg'
+  ];
+  const sliderImage = document.getElementById('sliderImage');
+  const moreSurpriseButton = document.getElementById('moreSurpriseButton');
 
-    function showNextImage() {
-      sliderImage.src = images[currentImage];
-      currentImage = (currentImage + 1) % images.length;
-    }
+  function showNextImage() {
+    sliderImage.src = images[currentImage];
+    currentImage = (currentImage + 1) % images.length;
+  }
 
+  showNextImage();
+  const intervalId = setInterval(showNextImage, 3000);
+
+  sliderImage.addEventListener('click', function() {
     showNextImage();
-    setInterval(showNextImage, 3000);
-
-    setTimeout(function() {
-      moreSurpriseButton.style.display = 'block';
-    }, images.length * 5000); // Show button after all images have been displayed
-
-    moreSurpriseButton.addEventListener('click', function() {
-      window.location.href = 'books.html';
-    });
   });
+
+  setTimeout(function() {
+    moreSurpriseButton.style.display = 'block';
+  }, images.length * 3000); // Show button after all images have been displayed
+
+  moreSurpriseButton.addEventListener('click', function() {
+    window.location.href = 'books.html';
+  });
+});
